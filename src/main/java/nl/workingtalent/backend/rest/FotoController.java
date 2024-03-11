36c6d546264ee8d4aa5e3fa073d1d4fa3ef4002d
Vanessa Dallaris <dallarisvanessa@yahoo.com>
2024-03-11 12:00:32 +0100

    online database

--- src/main/java/nl/workingtalent/backend/rest/FotoController.java
+++ src/main/java/nl/workingtalent/backend/rest/FotoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import nl.workingtalent.backend.model.Foto;
 import nl.workingtalent.backend.persist.FotoService;
 
 @RestController
+@CrossOrigin(maxAge = 3600)
 public class FotoController {
 	@Autowired
 	private FotoService fotoService;
