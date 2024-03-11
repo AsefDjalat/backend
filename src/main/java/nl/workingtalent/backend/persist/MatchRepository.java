dd94f511eaf766eec7f07a11d330bb15b70bd2db
Robin R.N. Koene <robinkoene@hotmail.com>
2024-03-11 10:27:24 +0100

    -update

--- src/main/java/nl/workingtalent/backend/persist/MatchRepository.java
+++ src/main/java/nl/workingtalent/backend/persist/MatchRepository.java
@@ -1,5 +1,6 @@
-package nl.workingtalent.backend;
+package nl.workingtalent.backend.persist;
 
+import nl.workingtalent.backend.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
--- src/main/java/nl/workingtalent/backend/persist/MatchService.java
+++ src/main/java/nl/workingtalent/backend/persist/MatchService.java
@@ -1,7 +1,12 @@
-package nl.workingtalent.backend;
+package nl.workingtalent.backend.persist;
 
+import nl.workingtalent.backend.model.Match;
+import nl.workingtalent.backend.model.Trainee;
+import nl.workingtalent.backend.model.Vacature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +15,10 @@ public class MatchService {
 
     @Autowired
     private MatchRepository repo;
+    @Autowired
+    private ITraineeRepository tRepo;
+    @Autowired
+    private IVacatureRepository vRepo;
 
     public Iterable<Match> findAllMatch(){return repo.findAll();}
 
@@ -22,12 +31,21 @@ public class MatchService {
     public List<Match> findMatchesByVacatureId(long vacatureId) {
         return repo.findByVacatureId(vacatureId);
     }
-    public Match createMatch(Match newMatch) {
-        Optional<Match> existingMatch = repo.findByTraineeIdAndVacatureId(newMatch.getTrainee().getId(), newMatch.getVacature().getId());
+    public Match createMatch(long tid, long vid) {
+        Trainee T = tRepo.findById(tid).get();
+        Vacature V = vRepo.findById(vid).get();
+        //hier de check of bestaat.
+        Optional<Match> existingMatch = repo.findByTraineeIdAndVacatureId(tid, vid);
         if (existingMatch.isPresent()) {
+            // If such a match exists, throw an exception or handle it as per your business logic
             throw new RuntimeException("A match with the given trainee and vacature IDs already exists.");
         }
-        return repo.save(newMatch);
+        Match match = new Match();
+        match.setTimestamp(LocalDateTime.now());
+        match.setTrainee(T);
+        match.setVacature(V);
+
+        return repo.save(match);
     }
 
     public void updateMatch(Match match){repo.save(match);}
